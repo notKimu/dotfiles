e480c1b851af4d4ff047f314700c052defffc2e7
notKimu <not.k1mu@gmail.com>
2023-11-24 20:10:43 +0100

    Added Inabakumori's theme

---
+++ INABAKUMORI/config/polybar/launch_polybar.sh
@@ -0,0 +1,9 @@
+pkill polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
---
+++ README.md
@@ -0,0 +1,2 @@
+# Kimu's dotfiles
+These are my collection of dotfiles, they include themes I've been doing over time and will get more polished over time, for now they are quite messy :p
